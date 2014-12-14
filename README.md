99d432d4a259824fb90fd9a1319f86b910451227
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2014-12-14 14:00:47 -0800

    fix formatting dings

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/Retro3223/frc2015.svg)](https://travis-ci.org/Retro3223/frc2015.svg)
+
 Retrobotic Bob is watching you
 
 ![alt tag](https://avatars0.githubusercontent.com/u/6465744?v=3&s=200)
--- robot/robot.py
+++ robot/robot.py
@@ -3,13 +3,17 @@ try:
 except ImportError:
     from pyfrc import wpilib
 
+
 class Robot(wpilib.SimpleRobot):
     def __init__(self):
         pass
+
     def Autonomous(self):
         pass
+
     def Disabled(self):
         pass
+
     def OperatorControl(self):
         pass
 
