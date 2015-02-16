e4ec7c1699a6c8d4f2d269f7f81d01c0b0f92f9c
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2015-02-15 17:28:23 -0800

    rewrite kiwidrive tests for pytest
    python isn't recognizing kiwidrive as a package, which is driving me
    bats

---
+++ kiwidrive/tests/tests.py
@@ -0,0 +1,57 @@
+import unittest
+import pytest
+import math
+from kiwidrive.robot import Robot
+from kiwidrive.kiwidrive import (
+    normalize_joystick_axes,
+    get_wheel_magnitudes,
+)
+
+
+joystick_magnitude_data = [(1.0, 1.0, 1.0),
+                           (0.0, 1.0, 1.0),
+                           (1.0, 0.0, 1.0),
+                           (1.0, 0.24, 1.0),
+                           (0.5, 0.0, 0.5),
+                           (0.0, 0.5, 0.5),
+                           (0.5, 0.5, 0.5),
+                           (-0.5, -1.0, 1.0),
+                           (-1.0, -1.0, 1.0),
+                           (0.0, 0.0, 0.0)]
+
+
+@pytest.mark.parametrize("input_x,input_y,expected_magnitude", joystick_magnitude_data)
+def test_normalize_joystick_axes_magnitudes(input_x, input_y, expected_magnitude):
+    (x, y) = normalize_joystick_axes(input_x, input_y)
+    assert round(expected_magnitude - math.hypot(x, y), 3) == 0
+
+
+joystick_axes_data = [(1.0, 1.0, 1/math.sqrt(2), 1/math.sqrt(2)),
+                      (-1.0, -1.0, -1/math.sqrt(2), -1/math.sqrt(2)),
+                      (1.0, -1.0, 1/math.sqrt(2), -1/math.sqrt(2)),
+                      (-1.0, 1.0, -1/math.sqrt(2), 1/math.sqrt(2)),
+                      (0.5, 0.0, 0.5, 0.0),
+                      (0.5, 1.0, 0.5/math.sqrt(1.25), 1/math.sqrt(1.25)),
+                      (-0.5, -1.0, -0.5/math.sqrt(1.25), -1/math.sqrt(1.25)),
+                      (0.0, 0.0, 0.0, 0.0)]
+
+@pytest.mark.parametrize("input_x,input_y,expected_x,expected_y", joystick_axes_data)
+def test_normalize_joystick_axes_values(input_x, input_y, expected_x, expected_y):
+    (x, y) = normalize_joystick_axes(input_x, input_y)
+    assert round(expected_x - x, 3) == 0
+    assert round(expected_y - y, 3) == 0
+
+
+wheel_magnitude_data = [(1.0, 0.0, -0.5, 1.0, 1.0),
+                        (-1.0, 0.0, 0.5, -1.0, -1.0),
+                        (0.0, 1.0, 0.0, -0.5773, 0.5773),
+                        (0.0, -1.0, 0.0, 0.5773, -0.5773),
+                        (0.0, 0.0, 0.0, 0.0, 0.0)]
+
+
+@pytest.mark.parametrize("input_x,input_y,expected_0,expected_1,expected_2", wheel_magnitude_data)
+def test_wheel_magnitidues(input_x, input_y, expected_0, expected_1, expected_2):
+    magnitudes = get_wheel_magnitudes((input_x, input_y))
+    assert round(expected_0 - magnitudes[0], 3) == 0
+    assert round(expected_1 - magnitudes[1], 3) == 0
+    assert round(expected_2 - magnitudes[2], 3) == 0
--- travis.sh
+++ travis.sh
@@ -6,4 +6,6 @@ elif [ $1 == "flake8-tiredrive" ]; then
     flake8 tiredrive
 elif [ $1 == "tiredrive-unittests"]; then
     python tiredrive/robot.py test tests.py
+elif [ $1 == "kiwidrive-unittests"]; then
+    python kiwidrive/robot.py test tests.py
 fi
